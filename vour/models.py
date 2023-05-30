2162e467ee6d1ed2c87cca3aa7635578de7d8e50
myusername <myemail>
2023-05-30 18:34:12 +0700

    perubahan dengan app

--- vour/models.py
+++ vour/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Career(models.Model):
+    career= models.CharField(max_length=50)
+    def __str__(self):
+        return self.career
+    
+    
+GENDER= (
+    (_)
+)
